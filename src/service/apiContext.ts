771e266fc22981aa3755a3b656ef544dd6d057e8
Hillson <hillsonshalom@gmail.com>
2024-10-31 09:32:20 +0200

    initial project

---
+++ src/service/apiContext.ts
@@ -0,0 +1,10 @@
+import { Mission } from "../types/MissionModel";
+
+export default class {
+    private baseUrl = "https://reactexambackend.onrender.com/";
+    private apikey = "8755316"
+
+    getAll = async ():Promise<Mission[]> => {
+
+    }
+}
